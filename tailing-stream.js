7eee31d070076ad67faff69c1c1e2a57e066efaa
Joe Garlick <joe.garlick@discsystems.co.uk>
2020-05-20 12:49:34 +0100

    Added: Close

--- tailing-stream.js
+++ tailing-stream.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var Readable = require('stream').Readable;
+var fs = require("fs");
+var Readable = require("stream").Readable;
 
 // copy properties from right-most args to left-most
 var extend = function (preserveExistingProperties) {
@@ -16,17 +16,19 @@ var extend = function (preserveExistingProperties) {
     for (prop in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, prop)) {
         // preserve preexisting child properties if specified
-        if (preserveExistingProperties &&
-            Object.prototype.hasOwnProperty.call(result, prop)) {
-              continue;
-            }
+        if (
+          preserveExistingProperties &&
+          Object.prototype.hasOwnProperty.call(result, prop)
+        ) {
+          continue;
+        }
         result[prop] = obj[prop];
       }
     }
   }
 
   return result;
-}
+};
 
 var TailingReadableStream = function () {
   // run Readable's init code on us, since we're a Readable
@@ -54,8 +56,8 @@ var TailingReadableStream = function () {
 TailingReadableStream.prototype = Object.create(Readable.prototype, {
   constructor: {
     value: TailingReadableStream,
-  enumerable: false
-  }
+    enumerable: false,
+  },
 });
 extend(true, TailingReadableStream, Readable);
 
@@ -66,17 +68,17 @@ TailingReadableStream.open = function (path, options) {
   var file = new TailingReadableStream();
 
   // override the timeout if present in options
-  if (Object.prototype.hasOwnProperty.call(options, 'timeout')) {
+  if (Object.prototype.hasOwnProperty.call(options, "timeout")) {
     file.timeout = options.timeout;
   }
 
   // set the reading start point if specified
-  if (Object.prototype.hasOwnProperty.call(options, 'start')) {
+  if (Object.prototype.hasOwnProperty.call(options, "start")) {
     file._offset = options.start;
   }
 
   // do not start emitting data events if specified and true
-  if (Object.prototype.hasOwnProperty.call(options, 'startPaused')) {
+  if (Object.prototype.hasOwnProperty.call(options, "startPaused")) {
     file._paused = options.startPaused;
   }
 
@@ -98,34 +100,35 @@ TailingReadableStream.prototype._watch = function () {
 
   // watch the file for changes
   this._watcher = fs.watch(this._path);
-  this._watcher.on('change', function (event, fileName) {
+  this._watcher.on("change", function (event, fileName) {
     // reset the kill switch for inactivity on every non-paused change
     self._resetTimeoutKillswitch();
 
     // start a new stream if one isn't running and a change happened
-    if (!self._stream && self.readable && event === 'change') {
+    if (!self._stream && self.readable && event === "change") {
       // send all data from the last byte sent to EOF
       var readOpts = extend(false, {}, self._read_stream_options);
       readOpts.start = self._offset;
       self._stream = fs.createReadStream(self._path, readOpts);
 
       // pipe data through our own event while tracking its progress
-      self._stream.on('data', function (data) {
+      self._stream.on("data", function (data) {
         // track the amount of data that's been read, then forward
         self._offset += data.length;
-        self.emit('data', data)
+        self.emit("data", data);
       });
 
       // forward errors and close the stream when received
-      self._stream.on('error', function (exception) {
+      self._stream.on("error", function (exception) {
         self._destroy();
-        self.emit('error', exception);
-        self.emit('close');
+        self.emit("error", exception);
+        self.emit("close");
       });
 
       // when we reach the end, destroy the stream and null it out so a
       // new one will be created for the next file change.
-      self._stream.on('end', function () {
+      self._stream.on("end", function () {
+        self._stream.close();
         self._stream.destroy();
         self._stream = null;
       });
@@ -133,20 +136,20 @@ TailingReadableStream.prototype._watch = function () {
   });
 
   // destroy then forward errors
-  this._watcher.on('error', function (exception) {
+  this._watcher.on("error", function (exception) {
     self._destroy();
-    self.emit('error', exception);
-    self.emit('close');
+    self.emit("error", exception);
+    self.emit("close");
   });
 
   // trigger initial change event to force streaming of existing file data
-  this._watcher.emit('change', 'change', this._path);
+  this._watcher.emit("change", "change", this._path);
 };
 
 // update the encoding of data sent to 'data' events
 TailingReadableStream.prototype.setEncoding = function (encoding) {
   // set the default
-  encoding = encoding || 'utf8';
+  encoding = encoding || "utf8";
 
   // update the read options
   this._read_stream_options.encoding = encoding;
@@ -200,8 +203,10 @@ TailingReadableStream.prototype._resetTimeoutKillswitch = function () {
 
   // set a new timeout unless timeout is disabled
   if (this.timeout) {
-    this._timeoutId = setTimeout(this._timeoutKillswitch.bind(this),
-        this.timeout);
+    this._timeoutId = setTimeout(
+      this._timeoutKillswitch.bind(this),
+      this.timeout
+    );
   }
 };
 
@@ -224,14 +229,14 @@ TailingReadableStream.prototype._destroy = function () {
 // shut down the stream and emit end and close events
 TailingReadableStream.prototype._timeoutKillswitch = function () {
   this._destroy();
-  this.emit('end');
-  this.emit('close');
+  this.emit("end");
+  this.emit("close");
 };
 
 // destroy the stream
 TailingReadableStream.prototype.destroy = function () {
   this._destroy();
-  this.emit('close');
+  this.emit("close");
 };
 
 // build a tailing readable stream given a path and some options
